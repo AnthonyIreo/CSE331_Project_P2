62f20794f4c9903eadc1d2ff4d297203a56567bf
Wenhui Huang <70555014+huangwenhui117@users.noreply.github.com>
2021-11-03 14:52:13 -0400

    Change1

--- Solution.java
+++ Solution.java
@@ -32,4 +32,21 @@ public class Solution {
         /* TODO: Your solution goes here */
         return sol;
     }
+    
+    private int outputPaths_Helper() {
+        clients.sort(new Comparator<Client>() {
+            @Override
+            public int compare(Client o1, Client o2) {
+                if (o1.payment > o2.payment) return -1;
+                else if (o1.payment < o2.payment) return 1;
+                else {
+                    if (o1.alpha < o2.alpha) return -1;
+                    else if (o1.alpha > o2.alpha) return 1;
+                    else return 0;
+                }
+            }
+        });
+        return 0;
+    }
+    
 }

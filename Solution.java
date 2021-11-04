acba5fd4fe4e2e89427c03db567a03721cfcf973
Wenhui Huang <70555014+huangwenhui117@users.noreply.github.com>
2021-11-04 17:09:29 -0400

    2

--- Solution.java
+++ Solution.java
@@ -1,6 +1,6 @@
 package ub.cse.algo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Solution {
 
@@ -29,24 +29,50 @@ public class Solution {
      */
     public SolutionObject outputPaths() {
         SolutionObject sol = new SolutionObject();
-        /* TODO: Your solution goes here */
-        return sol;
-    }
-    
-    private int outputPaths_Helper() {
         clients.sort(new Comparator<Client>() {
             @Override
             public int compare(Client o1, Client o2) {
-                if (o1.payment > o2.payment) return -1;
-                else if (o1.payment < o2.payment) return 1;
+                if (o1.alpha < o2.alpha) return -1;
+                else if (o1.alpha > o2.alpha) return 1;
                 else {
-                    if (o1.alpha < o2.alpha) return -1;
-                    else if (o1.alpha > o2.alpha) return 1;
-                    else return 0;
+                    if (o1.payment > o2.payment) return -1;
+                    else if (o1.payment < o2.payment) return 1;
+                    return 0;
                 }
             }
         });
+        int size_Client = clients.size();
+        for (Client client: clients) {
+            client.priority = size_Client;
+            size_Client--;
+        }
+        int contentProvider = graph.contentProvider;
+        HashMap<Integer, ArrayList<Integer>> paths = Traversals.bfsPaths(graph, clients);
+        HashMap<Integer, Integer> step_path = new HashMap<>(clients.size());
+        for (Client client: clients) {
+            client.priority = size_Client;
+            step_path.put(client.id, 0);
+            size_Client--;
+        }
+        HashMap<Integer, Queue<Client>> situ_Node = new HashMap<>(graph.size());
+        Queue<Client> startNode = new LinkedList<>() {
+             {
+                 addAll(clients);
+            }
+        };
+        startNode.addAll(clients);
+        situ_Node.put(contentProvider, startNode);
+        while (!situ_Node.isEmpty()) {
+
+        }
+
+        /* TODO: Your solution goes here */
+        return sol;
+    }
+
+    private int outputPaths_Helper() {
         return 0;
     }
-    
+
+
 }

4fa98269c67d8c6b22ea66684f5c57c6f8595808
Wenhui Huang <70555014+huangwenhui117@users.noreply.github.com>
2021-11-05 00:31:52 -0400

    update 12:31

--- Solution.java
+++ Solution.java
@@ -104,19 +104,98 @@ public class Solution {
         return sol;
     }
 
-    private int next_level(Packet packet) {
+    private void next_level(Packet packet, int currentNode) {
         int packetID = packet.client;
         ArrayList<Integer> path = packet.path;
         float tolerant = clientMap.get(packetID).alpha;
         int d = info.shortestDelays.get(packetID);
         int nextNode = path.get(locations_path.get(packetID));
-        int bandWidth_nextNode = bandwidths.get(nextNode);
-        int num_waitingPacket = situ_Node.get(nextNode).size();
-        if (level_waitingList.containsKey(nextNode)) {
-            num_waitingPacket += level_waitingList.get(nextNode).size();
-        }
+        int waitingTime = waitingPacket(nextNode, packet);
         double percentage = 0.4;
-        if ((int)(num_waitingPacket / bandWidth_nextNode) < (int)(tolerant * d * percentage))
-        return 0;
+        if (waitingTime > (int)(tolerant * d * percentage)) {
+            ArrayList<Integer> adjacentNode = graph.get(currentNode);
+            HashMap<Integer, Integer> waitingTimes = new HashMap<>();
+            adjacentNode.forEach(integer -> waitingTimes.put(integer, waitingPacket(integer, packet)));
+            int min_time = Collections.min(waitingTimes.values());
+            for (int nodeID: waitingTimes.keySet()) {
+                if (waitingTimes.get(nodeID) == min_time) nextNode = nodeID;
+            }
+
+            // update path in packet
+
+
+
+        }
+        level_waitingList.get(nextNode).add(packet);
+    }
+
+    private int waitingPacket(int nextNode, Packet packet) {
+        ArrayList<Packet> waitingList = new ArrayList<>() {
+            {
+                add(packet);
+                addAll(situ_Node.get(nextNode));
+                if (level_waitingList.containsKey(nextNode)) {
+                     addAll(level_waitingList.get(nextNode));
+                }
+                sort(new Comparator<Packet>() {
+                    @Override
+                    public int compare(Packet o1, Packet o2) {
+                        int pri_o1 = clientMap.get(o1.client).priority;
+                        int pri_o2 = clientMap.get(o2.client).priority;
+                        if (pri_o1 > pri_o2) return -1;
+                        else if (pri_o1 < pri_o2) return 1;
+                        return 0;
+                    }
+                });
+            }
+        };
+        int numWaiting = waitingList.indexOf(packet) + 1;
+        int bandWidth_nextNode = bandwidths.get(nextNode);
+        return numWaiting / bandWidth_nextNode;
+    }
+
+    private ArrayList<Integer> bfsPaths(Graph graph, ArrayList<Client> clients, int startNode, int endNode) {
+        /*
+            Initialize the prior array with -1 for storing the node
+            that is before the current one in the shortest paths
+         */
+        int[] priors = new int[graph.size()];
+        Arrays.fill(priors, -1);
+
+        // Run BFS, finding the nodes parent in the shortest path
+        Queue<Integer> searchQueue = new LinkedList<>();
+        searchQueue.add(startNode);
+        while (!searchQueue.isEmpty()) {
+            int node = searchQueue.poll();
+            for (int neighbor : graph.get(node)) {
+                if (priors[neighbor] == -1 && neighbor != graph.contentProvider) {
+                    priors[neighbor] = node;
+                    searchQueue.add(neighbor);
+                }
+            }
+        }
+
+        // Get the final shortest paths
+        HashMap<Integer, ArrayList<Integer>> paths = pathsFromPriors(clients, priors);
+        return paths.get(endNode);
+    }
+
+    private HashMap<Integer, ArrayList<Integer>> pathsFromPriors(ArrayList<Client> clients, int[] priors) {
+        HashMap<Integer, ArrayList<Integer>> paths = new HashMap<>(clients.size());
+        // For every client, traverse the prior array, creating the path
+        for (Client client : clients) {
+            ArrayList<Integer> path = new ArrayList<>();
+            int currentNode = client.id;
+            while (currentNode != -1) {
+                /*
+                    Add this ID to the beginning of the
+                    path so the path ends with the client
+                 */
+                path.add(0, currentNode);
+                currentNode = priors[currentNode];
+            }
+            paths.put(client.id, path);
+        }
+        return paths;
     }
 }
